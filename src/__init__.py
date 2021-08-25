042c8754ffad3ecea58e02bf2720533a4c537b84
潘嘉禾 <jiahe.pan@epfl.ch>
2021-08-25 17:41:45 +0200

    fixing bugs in parametric shifting and absolute timing

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .pulses import (Sweepable, sweepables, Carrier, Gaussian, Drag, Rect,
-                     Ramp, Cosine, sampling_frequency, relative_timing, phase_alignment)
-from .sequence import Sequence, set_trigger_delay
+                     Ramp, Cosine, config)
+from .sequence import Sequence
 from .utils.zhinst_awg_wrapper import update_zhinst_awg
 from .utils.delay_calibration import delay_calibration
