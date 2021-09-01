ec6da5b640512f18b36f31942fe578acd2b8d0aa
jiahe pan <jiahe.pan@epfl.ch>
2021-09-01 11:31:34 +0200

    minor fixes

--- src/seqpy/__init__.py
+++ src/seqpy/__init__.py
@@ -1,5 +1,3 @@
-from .pulses import (Sweepable, sweepables, Carrier, Gaussian, Drag, Rect,
+from .pulses import (sweepables, Carrier, Gaussian, Drag, Rect,
                      Ramp, Cosine, config)
 from .sequence import Sequence
-from .utils.zhinst_awg_wrapper import update_zhinst_awg
-from .utils.delay_calibration import delay_calibration
